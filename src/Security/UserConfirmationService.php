68dd14fbae59b667f1a2a2d04e71eabd0f9ca764
maciej <maciej.sawicky@gmail.com>
2019-08-07 21:14:00 +0200

    Added Monolog bundle and provided basic debug logs

--- src/Security/UserConfirmationService.php
+++ src/Security/UserConfirmationService.php
@@ -5,33 +5,41 @@ namespace App\Security;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Exception\InvalidConfirmationTokenException;
+use Psr\Log\LoggerInterface;
 
 class UserConfirmationService
 {
     private $userRepository;
     private $entityManager;
+    private $logger;
 
     public function __construct(
         UserRepository $userRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger
     )
     {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function confirmUser(string $confirmationToken)
     {
+        $this->logger->debug('Fetching user by confirmation token');
         $user = $this->userRepository->findOneBy(
             ['confirmationToken' => $confirmationToken]
         );
 
         if(!$user) {
+            $this->logger->debug('User by confirmation token was not found');
             throw new InvalidConfirmationTokenException();
         }
 
         $user->setEnabled(true);
         $user->setConfirmationToken(null);
         $this->entityManager->flush();
+
+        $this->logger->debug('Confirmed user by confirmation token.');
     }
 }
